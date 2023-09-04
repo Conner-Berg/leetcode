424ccf0e907a844256b5eb9071bdeb8e377f793b
Conner-Berg <connerwberg@gmail.com>
2023-09-04 11:12:47 -0400

    Add day 4 solution

---
+++ day-4-2665.js
@@ -0,0 +1,22 @@
+const createCounter = function (init) {
+	let current = init;
+
+	function increment() {
+		return ++current;
+	}
+	function decrement() {
+		return --current;
+	}
+	function reset() {
+		return (current = init);
+	}
+
+	return { increment, decrement, reset };
+};
+
+const counter = createCounter(5);
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.reset());
+console.log(counter.increment());
