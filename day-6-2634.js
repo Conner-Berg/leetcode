0a4dcda66e2a639bd60b5399662a5c28c38aae23
Conner-Berg <connerwberg@gmail.com>
2023-09-11 14:03:23 -0400

    Add day 6 solution

---
+++ day-6-2634.js
@@ -0,0 +1,25 @@
+function filter(arr, fn) {
+	let filteredArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (fn(arr[i], i)) filteredArr.push(arr[i]);
+	}
+	return filteredArr;
+}
+
+arr = [0, 10, 20, 30];
+fn = function greaterThan10(n) {
+	return n > 10;
+};
+console.log(filter(arr, fn));
+
+arr = [1, 2, 3];
+fn = function firstIndex(n, i) {
+	return i === 0;
+};
+console.log(filter(arr, fn));
+
+arr = [-2, -1, 0, 1, 2];
+fn = function plusOne(n) {
+	return n + 1;
+};
+console.log(filter(arr, fn));
