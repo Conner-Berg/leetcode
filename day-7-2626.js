3e30b62ebd9b8e71aa1ed2a90c493274293fe661
Conner-Berg <connerwberg@gmail.com>
2023-09-11 14:19:25 -0400

    Add day 7 solution

---
+++ day-7-2626.js
@@ -0,0 +1,28 @@
+function reduce(nums, fn, init) {
+	let accum = init;
+	for (let i = 0; i < nums.length; i++) {
+		accum = fn(accum, nums[i]);
+	}
+	return accum;
+}
+
+nums = [1, 2, 3, 4];
+fn = function sum(accum, curr) {
+	return accum + curr;
+};
+init = 0;
+console.log(reduce(nums, fn, init));
+
+nums = [1, 2, 3, 4];
+fn = function sum(accum, curr) {
+	return accum + curr * curr;
+};
+init = 100;
+console.log(reduce(nums, fn, init));
+
+nums = [];
+fn = function sum(accum, curr) {
+	return 0;
+};
+init = 25;
+console.log(reduce(nums, fn, init));
